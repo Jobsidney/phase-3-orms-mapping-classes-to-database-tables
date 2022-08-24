e1e94963f36e32de07d0f8ca073d1acadaae4500
Jobsidney <jobsidney67@gmail.com>
2022-08-24 09:25:22 +0300

    DOne

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,15 @@ Bundler.require
 require_relative '../lib/song'
 
 DB = { conn: SQLite3::Database.new("db/music.db") }
+Song.create_table
+hello = Song.new(name: "Hello", album: "25")
+hello.save
+ninety_nine_problems = Song.new(name: "99 Problems", album: "The Black Album")
+ninety_nine_problems.save
+song=Song.create(name: "Hello", album: "25")
+p song.name
+song.album
+
+
+
+p DB[:conn].execute("PRAGMA table_info(songs)")
